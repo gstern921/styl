ef4bf6c4d0c1384f9c553c15f72807e0e003b6ef
Gary Stern <gcrunkster@gmail.com>
2020-11-28 18:21:50 -0500

    Updated config settings

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H';
+  const publishableKey = "pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H";
 
-  const onToken = token => {
+  const onToken = (token) => {
     axios({
-      url: '/payment',
-      method: 'post',
+      url: "/payment",
+      method: "post",
       data: {
         amount: priceForStripe,
-        token: token
-      }
+        token: token,
+      },
     })
-      .then(response => {
-        alert('Succesful Payment!');
+      .then((response) => {
+        alert("Succesful Payment!");
       })
-      .catch(error => {
-        console.log('Payment Error: ', error);
+      .catch((error) => {
+        console.log("Payment Error: ", error);
         alert(
-          'There was an issue with your payment! Please make sure to use the provided credit card data'
+          "There was an issue with your payment! Please make sure to use the provided credit card data"
         );
       });
   };
 
   return (
     <StripeCheckout
-      label='Pay Now'
-      name='CRWN Clothing Ltd.'
+      label="Pay Now"
+      name="Styl Clothing Ltd."
       billingAddress
       shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
+      image="https://svgshare.com/i/CUz.svg"
       description={`Your total is $${price}`}
       amount={priceForStripe}
-      panelLabel='Pay Now'
+      panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishableKey}
     />
